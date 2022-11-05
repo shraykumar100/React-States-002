58756cf6c8205c25666093ee379b0c56b00b82f4
shray kumar <shraykumar100@gmail.com>
2022-11-05 22:01:51 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App () {
+
+  let str = '';
+
+  const newClicked = (event) => {
+    newOnpress( event.target.value );
+  }
+
+  const [ onPress, newOnpress ] = useState( str );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='maindiv'>
+      <div className='textarea'>
+        <textarea placeholder='-- Input Area --' onChange={ newClicked }></textarea>
+      </div>
+      <div className='markdownarea'>
+          <ReactMarkdown>
+            {onPress}
+          </ReactMarkdown>
+      </div>
     </div>
   );
 }
